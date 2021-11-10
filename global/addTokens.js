3686b8aecf107642bbe5aa0cdeb3b905bd5c1b31
Mar65gvcvhbjbn <94040355+Mar65gvcvhbjbn@users.noreply.github.com>
2021-11-10 18:17:28 +1030

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,6 +1,6 @@
 async function getName(authToken) {
     const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
-    const data = await response.json();
+    const data = await response.json(1);
 
     return data.name
 };
